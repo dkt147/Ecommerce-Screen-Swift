26f70f8305cb6060d106ebb7364ce64ceec83f80
Daniyal Khan <dk720820@gmail.com>
2021-12-29 13:43:21 +0500

    working on collection xib

---
+++ Table_CollectionView/View/Collections/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  Table_CollectionView
+//
+//  Created by JOEYCO-0072PK on 29/12/2021.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
